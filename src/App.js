e3a67d183e2dc3d258b25dad7b8318970e16e580
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-13 17:49:00 +0800

    refactor: Place MachineInputBox inside the components directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
-import MachineInputBox from './MachineInputBox';
+import MachineInputBox from './components/MachineInputBox';
 
 function App() {
   const [machine_specs, set_machine_specs] = useState("")
-  
+
+
+
   return (
     <MachineInputBox machine_specs={machine_specs} set_machine_specs={set_machine_specs}/>
   );
