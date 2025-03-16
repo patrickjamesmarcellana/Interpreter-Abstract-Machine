24c1adf7d088e5c523a671f851249c8df01fb4a6
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-17 02:17:19 +0800

    feat: Create initial state transition

--- src/machine_essentials/State.js
+++ src/machine_essentials/State.js
@@ -1,7 +1,11 @@
 class State {
-  constructor(name, is_initial=false) {
+  constructor(name, is_initial=false, is_final=false, transitions, is_accept=false, is_reject=false, destination_state=null) {
     this.name = name
     this.is_initial = is_initial
     this.is_final = is_final
+    this.transitions = []
+    this.is_accept = is_accept
+    this.is_reject= is_reject
+    this.destination_state = destination_state
   }
 }
