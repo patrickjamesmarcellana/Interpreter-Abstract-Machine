8227c7660df7a47a742e29be23cc74dd595184a6
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-17 06:23:16 +0800

    fix: Set the destination_state of a Transition object at a later time

--- src/machine_essentials/Transition.js
+++ src/machine_essentials/Transition.js
@@ -5,8 +5,11 @@ class Transition {
    * @param {function from TransitionList} action 
    * @param {State} destination_state 
    */
-  constructor(action, destination_state) {
+  constructor(action) {
     this.action = action
+  }
+
+  set_destination_state(destination_state) {
     this.destination_state = destination_state
   }
 }
