7ca80e24a46fcec713b53e6917aa522e12f49fe7
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-16 23:30:50 +0800

    docs: Add function descriptions for the scan and print functions in Transitions.js

--- src/machine_essentials/Transitions.js
+++ src/machine_essentials/Transitions.js
@@ -1,9 +1,23 @@
 // to use these, iterate through all saved lambda functions of a state by creating a new timeline 
 // per possible transition but excluding those that return false
+
+/**
+ * 
+ * @param {InputTape} input_tape 
+ * @param {char} scan_symbol 
+ * @returns true if scan_right was successful
+ *          false if otherwise
+ */
 function scan(input_tape, scan_symbol) {
-  return input_tape.scan(scan_symbol)
+  return input_tape.scan_right(scan_symbol)
 }
 
+/**
+ * 
+ * @param {OutputTape} output_tape 
+ * @param {char} print_symbol 
+ * @returns true after storing the print symbol in the output tape
+ */
 function print(output_tape, print_symbol) {
   output_tape.print(print_symbol)
   return true 
