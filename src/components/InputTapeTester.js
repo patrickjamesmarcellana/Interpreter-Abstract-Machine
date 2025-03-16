1f3c7727e2b1488f4ae540c05b6fa7cf46ad619e
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-17 01:30:20 +0800

    feat: Make the data structure a parameter instead to the InputTape and OutputTape testers

--- src/components/InputTapeTester.js
+++ src/components/InputTapeTester.js
@@ -1,9 +1,9 @@
 import InputTape from "../data_structures/InputTape"
 
-function InputTapeTester() {
-  const input_tape = new InputTape("I1", "00001111")
+function InputTapeTester(input_tape) {
   return (
     <div>
+      <h1>{input_tape.name}</h1>
       <button title="Scan 0 Right" onClick={() => input_tape.scan_right('0')}>Scan 0 Right</button>
       <button title="Scan 1 Right" onClick={() => input_tape.scan_right('1')}>Scan 1 Right</button>
       <button title="Scan # Right" onClick={() => input_tape.scan_right('#')}>Scan # Right</button>
@@ -13,7 +13,8 @@ function InputTapeTester() {
       <button title="Print Tape" onClick={() => input_tape.print_tape()}>Print Tape</button>
       <button title="Print Head" onClick={() => input_tape.print_head()}>Print Head</button>
       <button title="Is Complete" onClick={() => input_tape.is_complete()}>Is Complete</button>
-    </div> 
+      <br/>
+    </div>   
   )
 }
 
--- src/components/OutputTapeTester.js
+++ src/components/OutputTapeTester.js
@@ -1,13 +1,14 @@
 import OutputTape from "../data_structures/OutputTape"
 
-function OutputTapeTester() {
-  const output_tape = new OutputTape("O1")
+function OutputTapeTester(output_tape) {
   return (
     <div>
+      <h1>{output_tape.name}</h1>
       <button title="Print 0" onClick={() => output_tape.print('0')}>Print 0</button>
       <button title="Print 1" onClick={() => output_tape.print('1')}>Print 1</button>
       <button title="Print #" onClick={() => output_tape.print('#')}>Print #</button>
       <button title="Print Tape" onClick={() => output_tape.print_tape()}>Print Tape</button>
+      <br/>
     </div> 
   )
 }
