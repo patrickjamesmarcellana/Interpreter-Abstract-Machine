8806552fc1e00aaf74f2703fb44f007adcd597d9
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-17 01:57:32 +0800

    refactor: Reflect the method name changes of Stack adn Queue to their tester counterparts

--- src/components/QueueTester.js
+++ src/components/QueueTester.js
@@ -6,9 +6,9 @@ function QueueTester(queue) {
       <h1>{queue.name}</h1>
       <button title="Enqueue X" onClick={() => queue.write("X")}>Enqueue X</button>
       <button title="Enqueue Y" onClick={() => queue.write("Y")}>Enqueue Y</button>
-      <button title="Print Queue" onClick={() => queue.log_queue()}>Print Queue</button>
+      <button title="Print Queue" onClick={() => queue.log()}>Print Queue</button>
       <button title="Get Queue" onClick={() => console.log(queue.peek())}>Peek Queue</button>
-      <button title="Get In Front of Queue" onClick={() => console.log(queue.peek_in_front())}>Peek In Front of Queue</button>
+      <button title="Get In Front of Queue" onClick={() => console.log(queue.peek_next())}>Peek In Front of Queue</button>
       <button title="Dequeue Symbol" onClick={() => console.log(`Got symbol ${queue.read()}`)}>Dequeue</button>
       <br/>
     </div> 
--- src/components/StackTester.js
+++ src/components/StackTester.js
@@ -6,7 +6,7 @@ function StackTester(stack) {
       <h1>{stack.name}</h1>
       <button title="Push X" onClick={() => stack.write("X")}>Push X</button>
       <button title="Push Y" onClick={() => stack.write("Y")}>Push Y</button>
-      <button title="Print Stack" onClick={() => stack.log_stack()}>Print Stack</button>
+      <button title="Print Stack" onClick={() => stack.log()}>Print Stack</button>
       <button title="Get Stack" onClick={() => console.log(stack.peek())}>Peek Stack</button>
       <button title="Read Symbol" onClick={() => console.log(`Got symbol ${stack.read()}`)}>Pop</button>
       <br/>
