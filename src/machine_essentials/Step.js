45c2441c5af46bf6c5264deb729ff79fa415fb99
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-23 03:05:05 +0800

    feat: Add get_curr_state to Step

--- src/machine_essentials/Step.js
+++ src/machine_essentials/Step.js
@@ -5,9 +5,15 @@ class Step {
   constructor(curr_state, input_tape=null, memory_structures_map=null, output_tape=null, is_accepted=false, is_dead=false) {
     this.curr_state = curr_state
     this.input_tape = input_tape
-    this.output_tape = output_tape,
+    this.output_tape = output_tape
     this.memory_structures_map = memory_structures_map
     this.is_accepted = false
     this.is_dead = false
   }
+
+  get_curr_state() {
+    return this.curr_state
+  }
 }
+
+export default Step
