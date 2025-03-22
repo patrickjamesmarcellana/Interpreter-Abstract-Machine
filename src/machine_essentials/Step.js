38c64acbf594b6ceb6d5f66ca0f47301ab7a34c8
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-22 23:33:29 +0800

    feat: Convert Timeline.js to Step.js and create a new Timeline class that stores a list of steps

---
+++ src/machine_essentials/Step.js
@@ -0,0 +1,13 @@
+// Each timeline must have their own copy of all memory
+// Each timeline must have their own copy of the input tape
+
+class Step {
+  constructor(curr_state, input_tape=null, memory_structures_map=null, output_tape=null, is_accepted=false, is_dead=false) {
+    this.curr_state = curr_state
+    this.input_tape = input_tape
+    this.output_tape = output_tape,
+    this.memory_structures_map = memory_structures_map
+    this.is_accepted = false
+    this.is_dead = false
+  }
+}
