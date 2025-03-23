afe4fef5afb29aaf8392a44517c682a07da91b7f
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-03-23 16:44:57 +0800

    feat: Add form submission for StringInputBox

--- src/components/StringInputBox.js
+++ src/components/StringInputBox.js
@@ -1,12 +1,21 @@
 function StringInputBox({ input_string, set_input_string, is_machine_ready }) {
+  const handle_string_input_submission = (event) => {
+    event.preventDefault()
+    console.log(`String input submitted. Content: ${input_string}`)
+    // start machine simulation
+  }
+
   return(
     <div>
-      <input 
-        type="text" 
-        required={true} 
-        disabled={!is_machine_ready} 
-        placeholder="Enter input string" 
-        onChange={(e) => set_input_string(e.target.value)}/>
+      <form onSubmit={handle_string_input_submission}>
+        <input 
+          type="text" 
+          required={true} 
+          disabled={!is_machine_ready} 
+          placeholder="Enter input string" 
+          onChange={(e) => set_input_string(e.target.value)}/><br/>
+        <input type="submit"/>
+      </form>
     </div>
   )
 }
