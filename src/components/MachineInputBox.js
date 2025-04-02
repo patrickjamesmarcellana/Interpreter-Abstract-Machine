b73beb9b467b6a31d74659d8dc43d4a654b2dd35
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-04-02 16:47:45 +0800

    feat: Create a reset machine specs button

--- src/components/MachineInputBox.js
+++ src/components/MachineInputBox.js
@@ -4,10 +4,19 @@ function MachineInputBox({ machine_specs, set_machine_specs, set_is_input_string
 
   const [machine_submitted, set_machine_submitted] = useState(false)
 
+
   const handle_machine_submission = (event) => {
     event.preventDefault()
     set_is_machine_ready(true)
     set_machine_submitted(true)
+  }
+
+  const handle_reset_button_click = (event) => {
+    event.preventDefault()
+    set_is_input_string_ready(false)
+    set_is_machine_ready(false)
+    set_machine_submitted(false)
+    set_machine_specs("")
   }
 
   return (
@@ -23,17 +32,27 @@ function MachineInputBox({ machine_specs, set_machine_specs, set_is_input_string
             cols={85} 
             name="machine_specs"
             required={true}
+            value={machine_specs}
             placeholder="Enter machine specifications."
             onChange={(e) => {set_machine_specs(e.target.value); set_is_input_string_ready(false)}}
           />
       </label>
       <br/>
-      <input 
-        className={`disabled:cursor-not-allowed rounded-lg py-3 bg-[#008000] w-full drop-shadow-lg text-white hover:bg-gray-400 active:bg-gray-600 disabled:opacity-50 disabled:hover:bg-[#008000]`}
-        type="submit"
-        value="Submit Machine Specs"
-        disabled={machine_submitted}
-      />
+      <span>
+        <input 
+          className={`disabled:cursor-not-allowed rounded-lg py-3 bg-[#008000] w-[45%] drop-shadow-lg text-white hover:bg-gray-400 active:bg-gray-600 disabled:opacity-50 disabled:hover:bg-[#008000]`}
+          type="submit"
+          value="Submit Machine Specs"
+          disabled={machine_submitted}
+        />
+        <input 
+          className={`rounded-lg py-3 bg-[#c30010] w-[45%] float-right drop-shadow-lg text-white hover:bg-gray-400 active:bg-gray-600`}
+          type="button"
+          onClick={handle_reset_button_click}
+          value="Reset Machine Specs"
+        />
+      </span>
+      
     </form>
   )
 }
